b52a5eda228a56e149fcbae083a1f282aaced9c8
Pavlo Paitak <ppait@softserveinc.com>
2019-03-22 17:16:54 +0200

    Updated Values result

--- WebApplication/WebApplication/Controllers/ValuesController.cs
+++ WebApplication/WebApplication/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplication.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value1", "value2" };
         }
 
         // GET api/values/5
